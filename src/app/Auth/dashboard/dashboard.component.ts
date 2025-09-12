0f6053a83e63781df1dbbb2cbacb06aafc80b655
rubycrst <rubypereda456@gmail.com>
2025-09-12 18:08:37 -0500

    membresias

---
+++ src/app/Auth/dashboard/dashboard.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+import { AuthService } from '../../Core/services/auth.service';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
+})
+export class DashboardComponent {
+  user: any;
+
+  constructor(private auth: AuthService, private router: Router) {
+    this.user = this.auth.getCurrentUser();
+  }
+  
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
+}
