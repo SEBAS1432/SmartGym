05dab572f0fb863dbb538ccdc86ff43ccf3a0f4d
GeanFrancogg <limachigeanfranco@gmail.com>
2025-10-19 16:34:29 -0500

    Avances previos 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { Routes } from '@angular/router';
 import { InicioComponent } from './Auth/inicio/inicio.component';
 import { LoginComponent } from './Auth/login/login.component';
 import { DashboardComponent } from './Auth/dashboard/dashboard.component';
-import { authGuard } from './Core/guards/auth.guard';
-import { guestGuard } from './Core/guards/guest.guard';
 import { MembresiaComponent } from './Auth/membresia/membresia.component';
 import { ContactoComponent } from './Auth/contacto/contacto.component';
 import { RegistroComponent } from './Auth/registro/registro.component';
+import { ServiciosComponent } from './Auth/servicios/servicios.component';
+import { authGuard } from './Core/guards/auth.guard';
+import { guestGuard } from './Core/guards/guest.guard';
 
 export const routes: Routes = [
   { path: 'inicio', component: InicioComponent },
@@ -17,6 +18,10 @@ export const routes: Routes = [
 
   { path: 'membresia', component: MembresiaComponent },
 
+  { path: 'contacto', component: ContactoComponent },
+
+  { path: 'servicios', component: ServiciosComponent },
+
   {
     path: 'dashboard',
     loadComponent: () =>
@@ -24,14 +29,8 @@ export const routes: Routes = [
         (m) => m.DashboardComponent
       ),
   },
-  {
-    path: 'inicio',
-    loadComponent: () =>
-      import('./Auth/inicio/inicio.component').then(m => m.InicioComponent),
-  },
-  { path: 'contacto', component: ContactoComponent },
 
   { path: '', redirectTo: 'inicio', pathMatch: 'full' },
 
-  { path: '**', redirectTo: 'inicio' }
+  { path: '**', redirectTo: 'inicio' },
 ];
